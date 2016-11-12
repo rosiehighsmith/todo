cfa20d5d0f0f915dfd7eb8e5a47d6c32d54e3cb9
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-11-12 03:18:56 +0000

    Task API show tasks in JSON format

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe TasksController, type: :controller do
       task2 = FactoryGirl.create(:task)
       get :index
       expect(response).to have_http_status :success
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value.count).to eq(2)
     end
   end
 end
