187b4b44f7e56f5b814ee928e1c09b3e76268ff3
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-11-14 23:44:39 +0000

    Add strikethru for done items but remove strike if undone

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,7 +3,8 @@ $(function() {
   // and produces an HTML representation using <li> tags
   function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
-    var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id + '" class ="' + liClass + '">' + '<div class="view"><input class="toggle" type="checkbox"' +
       " data-id='" + task.id + "'" +
       checkedStatus +
       '><label>' +
@@ -26,6 +27,11 @@ $(function() {
       task: {
         done: doneValue
       }
+    }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
     });
   }
   // after successful page load, allow functions above
